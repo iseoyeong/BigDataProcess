1e696a892fc3d068ca4eadc9fcaa4cbf91fd95a6
이서영 <zjarhk21@naver.com>
2022-10-12 23:20:36 +0900

    Update HW2 code

--- HW2/student20180990.py
+++ HW2/student20180990.py
@@ -3,9 +3,8 @@
 import openpyxl
 
 wb = openpyxl.load_workbook("student.xlsx")
-ws = wb['sheet1']
+ws = wb['Sheet1']
 
-total_dict = dict()
 row_id = 1
 for row in ws:
 	if row_id != 1:
@@ -14,12 +13,5 @@ for row in ws:
 		total += ws.cell(row = row_id, column = 5).value * 0.34
 		total += ws.cell(row = row_id, column = 6).value 	
 		ws.cell(row = row_id, column = 7).value = total
-		total_dict[row_id] = total
-	#	print(total_dict)
 	row_id += 1
 wb.save("student.xlsx")
-
-sorted_dict = sorted(total_dict.items(), key = lambda item: item[1], reverse=True)
-print(sorted_dict)
-
-print(len(sorted_dict))
